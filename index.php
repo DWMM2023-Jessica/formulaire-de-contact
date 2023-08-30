c6b1e614b3b51cf5a87b12bab66833481f2c03ab
Moulun <Dylan.moulun59@outlook.fr>
2023-08-30 15:45:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com%22%3E/">
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Playfair+Display&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <footer>
+        <div class="color_Fond">
+            <h1 class="section_formtitre">CONTACTEZ-NOUS</h1>
+            <div class="row">
+                <div class="form">
+                    <form action="" METHOD="POST">
+                        <label class="Formulaire_info" for="name">Nom:</label>
+                        <input class="formaulaire_btn" type="text" name="Nom"><br><br>
+
+                        <label class="Formulaire_info" for="Prenom">Prenom:</label>
+                        <input class="formaulaire_btn" type="text" name="Prenom"><br><br>
+
+                        <label class="Formulaire_info" for="Adresse">Adresse e-mail: </label>
+                        <input class="formaulaire_btn" type="text" name="Adresse"><br><br>
+
+                        <label class="Formulaire_info" for="numero">Votre numero de telephone:</label>
+                        <input class="formaulaire_btn" type="text" name="numero">
+                    </form>
+
+                <div class="form_commentaire">
+
+                    <p class="messages">Messages:</p>
+                    <div class="commentaire"></div>
+                        <div class="conditiondiv">
+                            <input class="checkbox" type="checkbox">
+                                <p class="condition">En soumettant ce formulaire, j'accepte que mes données personnelles soient utilisées pour me recontacter. Aucun autre traitement ne sera effectué avec mes informations. </p>
+                        </div>
+                        <input class="envoyer" type="submit" name="envoyer" value="Envoyer le message">
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
